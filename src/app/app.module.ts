fce43bb5a65f4b26f2fe149c6f05b6224cf6e222
Nicolas Tapia <ntapia.dev@gmail.com>
2023-08-13 12:54:32 +0200

    A propos de nos formations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { AreasComponent } from './components/areas/areas.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -16,6 +18,8 @@ import { TrainingsComponent } from './pages/trainings/trainings.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
+    AboutComponent,
+    AreasComponent,
     TrainingsComponent,
     TrainingComponent,
   ],
