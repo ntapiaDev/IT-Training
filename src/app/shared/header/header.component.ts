0e60020e6c479e457fc7d0be0b8b4e6df5c787b2
Nicolas Tapia <ntapia.dev@gmail.com>
2023-08-25 16:23:10 +0200

    Ajout du bouton

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,9 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isBurgerOpen: boolean = false;
 
+  toggleBurger() {
+    this.isBurgerOpen = !this.isBurgerOpen;
+  }
 }
