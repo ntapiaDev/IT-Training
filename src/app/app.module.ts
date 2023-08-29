77b74d238677391ca20c3e4ce2ad53df140026d0
zelermehdi <gamouh.mehdi@gmail.com>
2023-08-29 11:56:03 +0200

    auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'; 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
@@ -12,6 +13,7 @@ import { HomeModule } from './modules/home/home.module';
 import { TrainingsModule } from './modules/trainings/trainings.module';
 import { FooterComponent } from './shared/footer/footer.component';
 import { SessionEffects } from './core/effects/session.effects';
+import { AuthInterceptor } from "./core/interceptors/auth.interceptor";
 import { areasReducer } from './core/stores/areas/areas.reducer';
 import { sessionReducer } from './core/stores/session/session.reducer';
 import { trainingsReducer } from './core/stores/trainings/trainings.reducer';
@@ -25,6 +27,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule,
     HomeModule,
     TrainingsModule,
@@ -37,7 +40,7 @@ import { SharedModule } from './shared/shared.module';
     }),
     SharedModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [
     AppComponent
   ]
--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterFormComponent } from './components/register/register-form.component';
 
 const routes: Routes = [
     { path: '', component: LandingComponent },
+    { path: 'connexion', component: LoginComponent },
+    { path: 'inscription', component: RegisterFormComponent },
 ];
 
 @NgModule({
